d809c91ed250f7eab0091ad8b4d585182f328b92
ctwarit1 <ctwarit1@gmail.com>
2022-10-31 21:28:29 +0530

    added: using switch case

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -3,23 +3,26 @@ package com.bridgelabz;
 public class EmployeeWage {
 	public static void main(String[] args){
 		// Constants
-		int IS_PART_TIME = 1;
-		int IS_FULL_TIME =2;
+		int IS_PART_TIME = 0;
+		int IS_FULL_TIME =1;
 		int EMP_RATE_PER_HOUR = 20;
 		int empHrs = 0;
 		int empWage = 0;
 		// Computation
 		double empCheck=Math.floor(Math.random()*10)%2;
-		if(empCheck==IS_PART_TIME)
+		switch((int) empCheck) 
+		{
+		case 0:
 			empHrs =4;
-		else if(empCheck==IS_FULL_TIME)
-			empHrs = 8 ;
-		else
+			break;
+		case 1:
+			empHrs = 8;
+			break;
+		default:
 			empHrs=0;
+		}
 		empWage=empHrs*EMP_RATE_PER_HOUR;
 		System.out.println("emp wage: "+empWage);
-			
-//			System.out.println("Employee is Absent");
-		
+				
 	}
 }
