70dbe29a3e1eac7d5795227ae3c17540e5a79d71
ctwarit1 <ctwarit1@gmail.com>
2022-10-31 20:57:46 +0530

    added: part time employee and wage

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -3,12 +3,23 @@ package com.bridgelabz;
 public class EmployeeWage {
 	public static void main(String[] args){
 		// Constants
-		int IS_FULL_TIME =1;
+		int IS_PART_TIME = 1;
+		int IS_FULL_TIME =2;
+		int EMP_RATE_PER_HOUR = 20;
+		int empHrs = 0;
+		int empWage = 0;
 		// Computation
 		double empCheck=Math.floor(Math.random()*10)%2;
-		if(empCheck==IS_FULL_TIME)
-			System.out.println("Employee is Present");
+		if(empCheck==IS_PART_TIME)
+			empHrs =4;
+		else if(empCheck==IS_FULL_TIME)
+			empHrs = 8 ;
 		else
-			System.out.println("Employee is Absent");
+			empHrs=0;
+		empWage=empHrs*EMP_RATE_PER_HOUR;
+		System.out.println("emp wage: "+empWage);
+			
+//			System.out.println("Employee is Absent");
+		
 	}
 }
